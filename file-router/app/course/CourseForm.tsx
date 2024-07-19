fc9fa82dd79165850ce6f54cde76591311a64dcf
Leo Lee <ireung2001@gmail.com>
2024-07-19 17:14:02 +1000

    tailwind css design

--- file-router/app/course/CourseForm.tsx
+++ file-router/app/course/CourseForm.tsx
@@ -5,9 +5,11 @@ const CourseForm = () => {
     console.log("Hello World");
   }
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={onSubmit} className="flex flex-col p-12 gap-4">
       <input type="text" name="name" className="border"></input>
-      <button type="submit">Submit</button>
+      <button type="submit" className="btn">
+        Submit
+      </button>
     </form>
   );
 };
